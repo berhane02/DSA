2cbaa8b4aca61f737a4b2d7d78be9b7db6a59b6e
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-04 00:03:49 -0400

    Time: 4 ms (52.69%) | Memory: 12.5 MB (59.87%) - LeetSync

--- 389-find-the-difference/find-the-difference.py
+++ 389-find-the-difference/find-the-difference.py
@@ -5,13 +5,18 @@ class Solution(object):
         :type t: str
         :rtype: str
         """
-        char_count = Counter(s)
+        # char_count = Counter(s)
       
-        # Iterate through each character in string t
-        for char in t:
-            # Decrement the count for this character
-            char_count[char] -= 1
+        # # Iterate through each character in string t
+        # for char in t:
+        #     # Decrement the count for this character
+        #     char_count[char] -= 1
           
-            # If count becomes negative, this is the extra character added to t
-            if char_count[char] < 0:
-                return char
+        #     # If count becomes negative, this is the extra character added to t
+        #     if char_count[char] < 0:
+        #         return char
+
+        c = 0
+        for cs in s: c^= ord(cs)
+        for ct in t: c^= ord(ct)
+        return chr(c)
