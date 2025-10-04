f37a8fafba99b01fe0f361a43140cee4f684f8cf
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-03 23:58:16 -0400

    Time: 10 ms (16.72%) | Memory: 12.4 MB (89.66%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        char_count = Counter(s)
+      
+        # Iterate through each character in string t
+        for char in t:
+            # Decrement the count for this character
+            char_count[char] -= 1
+          
+            # If count becomes negative, this is the extra character added to t
+            if char_count[char] < 0:
+                return char
