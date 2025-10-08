34c45480dbf45c6c5585b13007d66dd5457bfd63
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-07 21:19:24 -0400

    Time: 24 ms (38.37%) | Memory: 12.7 MB (53.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,15 +5,17 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        # s1 = Counter(s)
-        # t1 = Counter(t)
-        # return s1 == t1
-        counter_s = [0]*26
-        counter_t = [0]*26
+        # if len(s) != len(t):
+        #     return False
+        # # s1 = Counter(s)
+        # # t1 = Counter(t)
+        # # return s1 == t1
+        # counter_s = [0]*26
+        # counter_t = [0]*26
 
-        for i in range(len(s)):
-            counter_s [ord(s[i])-ord('a')] +=1
-            counter_t [ord(t[i])-ord('a')] +=1
-        return counter_s == counter_t
+        # for i in range(len(s)):
+        #     counter_s [ord(s[i])-ord('a')] +=1
+        #     counter_t [ord(t[i])-ord('a')] +=1
+        # return counter_s == counter_t
+
+        return Counter(s) == Counter(t)
