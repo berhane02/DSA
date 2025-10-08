8595302c4d82bfedfd4548fbf95dc3a3c1365364
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-07 21:14:03 -0400

    Time: 19 ms (69.26%) | Memory: 14.5 MB (10.01%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+        # s1 = Counter(s)
+        # t1 = Counter(t)
+        # return s1 == t1
+        counter_s = [0]*26
+        counter_t = [0]*26
+
+        for i in range(len(s)):
+            counter_s [ord(s[i])-ord('a')] +=1
+            counter_t [ord(t[i])-ord('a')] +=1
+        return counter_s == counter_t
