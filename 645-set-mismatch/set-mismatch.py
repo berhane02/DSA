6c8d7f8b20608cbb698e2296a8c00119819048f1
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-08 23:58:13 -0400

    Time: 3 ms (95.79%) | Memory: 13.9 MB (54.48%) - LeetSync

--- 645-set-mismatch/set-mismatch.py
+++ 645-set-mismatch/set-mismatch.py
@@ -5,12 +5,18 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        s = set()
-        duplicate = 0
-        for n in nums:
-            if n in s:
-                duplicate = n
-            s.add(n)
-        for i in range(len(nums)):
-            if i+1 not in s:
-                return [duplicate, i+1]
+        # s = set()
+        # duplicate = 0
+        # for n in nums:
+        #     if n in s:
+        #         duplicate = n
+        #     s.add(n)
+        # for i in range(len(nums)):
+        #     if i+1 not in s:
+        #         return [duplicate, i+1]
+
+        n, a, b = len(nums), sum(nums), sum(set(nums))
+		
+        s = n*(n+1)//2
+        
+        return [a-b, s-b]
