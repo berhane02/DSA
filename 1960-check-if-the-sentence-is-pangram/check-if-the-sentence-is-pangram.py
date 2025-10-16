87a3e27a0c5bf1365bc272a3855dcdd7d9dfc15d
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-16 00:45:23 -0400

    Time: 0 ms (100.00%) | Memory: 17.8 MB (51.18%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.py
@@ -1,10 +1,6 @@
 class Solution:
     def checkIfPangram(self, sentence: str) -> bool:
         present = set()
-
         for c in sentence:
             present.add(c)
-        # alp = set('abcdefghijklmnopqrstuvwxyz')
-        # dif = alp - present
-        # print(len(present))
         return len(present) == 26
