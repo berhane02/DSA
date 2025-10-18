3752004d1f9981dbb0bda091b7ac77c915106321
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-17 23:36:38 -0400

    Time: 3 ms (87.73%) | Memory: 19.2 MB (51.01%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        g = defaultdict(list)
+        for a,b in prerequisites:
+                g[a].append(b)
+        notvisited, visiting, visited = 0,1,2
+
+        status = [notvisited]*numCourses
+
+        def dfs(i):
+            
+            if status[i] == visiting: return False
+            if status[i] == visited: return True
+
+            status[i] = visiting
+
+            for n in g[i]:
+                if dfs(n) == False: return False
+            status[i] = visited
+            return True
+        
+        for i in range(numCourses):
+            if dfs(i) is not True:
+                return False
+        return True
