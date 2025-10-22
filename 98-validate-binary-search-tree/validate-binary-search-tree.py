6c95d3942d7b637af9401ea4443c73d406aab8fa
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-10-22 19:23:05 -0400

    Time: 0 ms (100.00%) | Memory: 19.8 MB (30.13%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/validate-binary-search-tree.py
@@ -8,22 +8,24 @@ class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
         
 
-        # def isValid(node, min, max):
+        def isValid(node, min, max):
             
-        #     if not node: return True
+            if not node: return True
 
-        #     if node.val <= min or node.val >= max:
-        #         return False
-
-        #     return (isValid(node.left, min, node.val) and isValid(node.right, node.val, max))
-        # return isValid(root, -inf, inf)
-
-        def inorder(node):
-            if not node: return []
-            return inorder(node.left) + [node.val] + inorder(node.right)
-        listTree = inorder(root)
-
-        for i in range(1, len(listTree)):
-            if listTree[i] <= listTree[i-1]:
+            if node.val <= min or node.val >= max:
                 return False
-        return True
+
+            return (isValid(node.left, min, node.val) and isValid(node.right, node.val, max))
+        return isValid(root, -inf, inf)
+
+        # def inorder(node):
+        #     if not node: return []
+        #     return inorder(node.left) + [node.val] + inorder(node.right)
+        # listTree = inorder(root)
+
+        # for i in range(1, len(listTree)):
+        #     if listTree[i] <= listTree[i-1]:
+        #         return False
+        # return True
+
+
