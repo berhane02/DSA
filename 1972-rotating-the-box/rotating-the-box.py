afe782f60c2e9458aa016807116b1b6abfa6c6d8
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-11-26 23:32:54 -0500

    Time: 139 ms (59.77%) | Memory: 62.9 MB (69.58%) - LeetSync

---
+++ 1972-rotating-the-box/rotating-the-box.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rotateTheBox(self, boxGrid: List[List[str]]) -> List[List[str]]:
+        m, n = len(boxGrid), len(boxGrid[0])
+
+        # Step 1: apply gravity
+        for r in range(m):
+            empty = n - 1
+            for c in range(n - 1, -1, -1):
+                if boxGrid[r][c] == '*':
+                    empty = c - 1
+                elif boxGrid[r][c] == '#':
+                    if empty != c:
+                        boxGrid[r][empty], boxGrid[r][c] = boxGrid[r][c], '.'
+                    empty -= 1
+
+        # Step 2: rotate clockwise —— result is n x m
+        res = [[None] * m for _ in range(n)]
+        for r in range(m):
+            for c in range(n):
+                res[c][m - 1 - r] = boxGrid[r][c]
+
+        return res
