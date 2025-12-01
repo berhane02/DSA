4e5f046ee6ec0f834cd433bf2bd6a7f779b3f81b
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-11-30 23:50:11 -0500

    Time: 39 ms (42.21%) | Memory: 17.9 MB (16.38%) - LeetSync

--- 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
+++ 2288-count-operations-to-obtain-zero/count-operations-to-obtain-zero.py
@@ -5,8 +5,7 @@ class Solution:
         while num1>0 and num2 > 0:
             if num1>=num2:
                 num1 = num1-num2
-                count +=1
-            if num1 !=0 and num2>num1:
+            else:
                 num2 = num2-num1
-                count +=1
+            count +=1
         return count
