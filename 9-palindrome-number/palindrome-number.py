d9d2e53ec2c8bd80e1d3715c95ef999768ba6ba6
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-12-05 23:52:27 -0500

    Time: 8 ms (51.94%) | Memory: 17.6 MB (98.29%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        num = str(x)
+        l1,l2 = 0, len(num)-1
+
+        while l1<=l2:
+
+            if num[l1] != num[l2]:
+                return False
+            l1+=1
+            l2-=1
+        return True
