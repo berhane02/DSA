6099b3313b584877bc84b193103d4b09063eec1c
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2025-12-26 23:56:36 -0500

    Time: 15 ms (5.62%) | Memory: 17.4 MB (91.97%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.py
+++ 2-add-two-numbers/add-two-numbers.py
@@ -1,30 +1,25 @@
 # Definition for singly-linked list.
-# class ListNode(object):
+# class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution(object):
-    def addTwoNumbers(self, l1, l2):
-        """
-        :type l1: Optional[ListNode]
-        :type l2: Optional[ListNode]
-        :rtype: Optional[ListNode]
-        """
-        result = ListNode(0)
-        curry = 0
-        curr = result
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
 
-        while l1 or l2 or curry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
-            
-            total = val1+val2+curry
-            curry = total//10
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
-            curr.next = ListNode(total%10)
+            total = v1 + v2 + carry
+            carry = total // 10
+
+            curr.next = ListNode(total % 10)
             curr = curr.next
- 
-            # move to next nodes
+
             if l1: l1 = l1.next
-            if l2: l2 = l2.next 
-        return result.next
+            if l2: l2 = l2.next
+
+        return dummy.next
