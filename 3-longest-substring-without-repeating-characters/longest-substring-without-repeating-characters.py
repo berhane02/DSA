b467c7c069805a35719a06b02065cc54c6e48017
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2026-01-14 23:57:05 -0500

    Time: 8 ms (98.42%) | Memory: 12.6 MB (74.46%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -8,7 +8,7 @@ class Solution(object):
         left = max_len = 0
 
         for right, char in enumerate(s):
-            # If char seen before, move left pointer past its last index
+            # If char seen before, move left pointer past its last
             if char in seen and seen[char] >= left:
                 left = seen[char] + 1
             seen[char] = right
