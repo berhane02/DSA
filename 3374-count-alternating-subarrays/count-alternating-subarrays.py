137b6a2bfd5771943c6164377e915a93370667c8
Yemane Berhane <44740293+berhane02@users.noreply.github.com>
2026-05-14 23:00:35 -0400

    Time: 55 ms (81.38%) | Memory: 24.5 MB (56.38%) - LeetSync

--- 3374-count-alternating-subarrays/count-alternating-subarrays.py
+++ 3374-count-alternating-subarrays/count-alternating-subarrays.py
@@ -1,14 +1,25 @@
 class Solution:
     def countAlternatingSubarrays(self, nums: List[int]) -> int:
-        ans = 0
-        length = 1
+        count = 1
 
-        ans = 1
+        curr = 1
+
+        
+
         for i in range(1, len(nums)):
-            if nums[i] != nums[i - 1]:
-                length += 1
-            else:
-                length = 1
-            ans += length
 
-        return ans
+            if nums[i] != nums[i - 1]:
+
+                curr += 1
+
+            else:
+
+                curr = 1
+
+            
+
+            count += curr
+
+        
+
+        return count
